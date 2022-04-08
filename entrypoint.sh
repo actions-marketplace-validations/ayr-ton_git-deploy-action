6a307c51786b0e72fccb95f19a20f94f34b0b18e
Ayrton <ayrton@riseup.net>
2022-04-08 16:36:55 -0300

    Fix if statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,9 @@ mkdir -p /root/.ssh
 echo "$INPUT_SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 
-if [[ ! -v INPUT_TARGET_BRANCH ]]; then
-    git checkout $INPUT_TARGET_BRANCH
+if ${INPUT_TARGET_BRANCH+"false"}
+then
+   git checkout $INPUT_TARGET_BRANCH
 fi
 
 git remote add destination "$INPUT_TARGET_REPO_URL"
