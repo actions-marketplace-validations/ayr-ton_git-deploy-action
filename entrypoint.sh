b8aa05b9a171248f2ab2126030e949d1f798e315
Ayrton <ayrton@riseup.net>
2022-04-08 16:39:49 -0300

    Fix checkout statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,12 @@ mkdir -p /root/.ssh
 echo "$INPUT_SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 
+git remote add destination "$INPUT_TARGET_REPO_URL"
+
 if ${INPUT_TARGET_BRANCH+"false"}
 then
    git checkout $INPUT_TARGET_BRANCH
+   git push destination "$INPUT_TARGET_BRANCH:$INPUT_TARGET_BRANCH" -f
+else
+   git push destination "$GITHUB_REF:$GITHUB_REF" -f
 fi
-
-git remote add destination "$INPUT_TARGET_REPO_URL"
-git push destination "$GITHUB_REF:$GITHUB_REF" -f
